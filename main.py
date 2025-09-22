208fa752d8f68eea0ff2a79b48edba4cd3366953
Yuttana Emyong <68130023@dpu.ac.th>
2025-09-22 21:32:15 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from mylib import myfunction as mf
+x = input("ใส่ตัวอักษร = ")
+y = int(input("How many turns you want to run = "))
+mf(x,y)
---
+++ mylib.py
@@ -0,0 +1,6 @@
+def myfunction(x, y):
+    i = 1
+    while i <= y:
+        result = x * i
+        print(result)
+        i += 1
